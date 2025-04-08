c75a0cadf028686e4341589eba69040407d2bf3e
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 01:45:20 +0200

    Update bargain_shop.asm

--- data/items/bargain_shop.asm
+++ data/items/bargain_shop.asm
@@ -1,10 +1,10 @@
 BargainShopData:
 	db 7
-	dbw NUGGET,       4500
-	dbw BIG_NUGGET,   8500
-	dbw PEARL,         650
-	dbw BIG_PEARL,    3500
-	dbw PEARL_STRING, 6500
+	dbw NUGGET,       2000
+	dbw BIG_NUGGET,   5000
+	dbw PEARL,        1000
+	dbw BIG_PEARL,    2000
+	dbw PEARL_STRING, 3000
 	dbw STARDUST,     1000
-	dbw STAR_PIECE,   4600
+	dbw STAR_PIECE,   2000
 	db -1
