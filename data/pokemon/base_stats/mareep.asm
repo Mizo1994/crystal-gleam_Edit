3e981c9f9b695582bd06270de78276ef0997d841
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:23:27 +0200

    Update mareep.asm

--- data/pokemon/base_stats/mareep.asm
+++ data/pokemon/base_stats/mareep.asm
@@ -5,7 +5,7 @@
 	db 235 ; catch rate
 	db 59 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 if DEF(FAITHFUL)
 	abilities_for MAREEP, STATIC, STATIC, STATIC
