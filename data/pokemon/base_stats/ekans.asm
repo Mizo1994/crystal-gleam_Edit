da0d769ae4621648234bd7d8ac53066f24d901c1
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:15:18 +0200

    Update ekans.asm

--- data/pokemon/base_stats/ekans.asm
+++ data/pokemon/base_stats/ekans.asm
@@ -5,7 +5,7 @@
 	db 255 ; catch rate
 	db 62 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for EKANS, INTIMIDATE, SHED_SKIN, UNNERVE
 	db GROWTH_MEDIUM_FAST ; growth rate
