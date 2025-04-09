b7f1096b1a5cdbb5ac008dacdff442664bcc9a57
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:17:10 +0200

    Update geodude_plain.asm

--- data/pokemon/base_stats/geodude_plain.asm
+++ data/pokemon/base_stats/geodude_plain.asm
@@ -5,7 +5,7 @@
 	db 255 ; catch rate
 	db 86 ; base exp
 	db NO_ITEM, EVERSTONE ; held items
-	dn GENDER_F50, HATCH_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for GEODUDE, ROCK_HEAD, STURDY, SAND_VEIL
 	db GROWTH_MEDIUM_SLOW ; growth rate
