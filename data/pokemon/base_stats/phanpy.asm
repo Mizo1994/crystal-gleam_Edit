49228a7dcd71d226f9e33b0b5143f29da257bd1c
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:27:33 +0200

    Update phanpy.asm

--- data/pokemon/base_stats/phanpy.asm
+++ data/pokemon/base_stats/phanpy.asm
@@ -5,7 +5,7 @@
 	db 120 ; catch rate
 	db 124 ; base exp
 	db NO_ITEM, KEE_BERRY ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for PHANPY, PICKUP, PICKUP, SAND_VEIL
 	db GROWTH_MEDIUM_FAST ; growth rate
