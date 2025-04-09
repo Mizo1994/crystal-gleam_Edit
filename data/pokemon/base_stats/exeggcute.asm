d1cbbc0bbcf3a06b3fed5250e2ec340482e19b9b
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:15:33 +0200

    Update exeggcute.asm

--- data/pokemon/base_stats/exeggcute.asm
+++ data/pokemon/base_stats/exeggcute.asm
@@ -5,7 +5,7 @@
 	db 90 ; catch rate
 	db 98 ; base exp
 	db NO_ITEM, MARANGABERRY ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for EXEGGCUTE, CHLOROPHYLL, CHLOROPHYLL, HARVEST
 	db GROWTH_SLOW ; growth rate
