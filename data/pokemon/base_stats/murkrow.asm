47b111c954df3b690c279c9d50903e96dc684633
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:26:35 +0200

    Update murkrow.asm

--- data/pokemon/base_stats/murkrow.asm
+++ data/pokemon/base_stats/murkrow.asm
@@ -5,7 +5,7 @@
 	db 30 ; catch rate
 	db 107 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for MURKROW, INSOMNIA, SUPER_LUCK, PRANKSTER
 	db GROWTH_MEDIUM_SLOW ; growth rate
