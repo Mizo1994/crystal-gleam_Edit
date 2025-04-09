4a00ca414b323a5cf1620c0013104254dab22c42
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:27:27 +0200

    Update paras.asm

--- data/pokemon/base_stats/paras.asm
+++ data/pokemon/base_stats/paras.asm
@@ -5,7 +5,7 @@
 	db 190 ; catch rate
 	db 70 ; base exp
 	db TINYMUSHROOM, BIG_MUSHROOM ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for PARAS, EFFECT_SPORE, DRY_SKIN, DAMP
 	db GROWTH_MEDIUM_FAST ; growth rate
