238da271501c5d45ff6ed5b9f21628572e26eaa1
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:29:31 +0200

    Update seel.asm

--- data/pokemon/base_stats/seel.asm
+++ data/pokemon/base_stats/seel.asm
@@ -5,7 +5,7 @@
 	db 190 ; catch rate
 	db 100 ; base exp
 	db NO_ITEM, RAWST_BERRY ; held items
-	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for SEEL, THICK_FAT, HYDRATION, ICE_BODY
 	db GROWTH_MEDIUM_FAST ; growth rate
