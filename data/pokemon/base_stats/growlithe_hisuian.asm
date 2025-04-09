dcf4acc88046d3fc15b194321a3315ac991a31eb
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:17:53 +0200

    Update growlithe_hisuian.asm

--- data/pokemon/base_stats/growlithe_hisuian.asm
+++ data/pokemon/base_stats/growlithe_hisuian.asm
@@ -5,7 +5,7 @@
 	db 190 ; catch rate
 	db 91 ; base exp
 	db ALWAYS_ITEM_2, ASPEAR_BERRY ; held items
-	dn GENDER_F25, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F25, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for GROWLITHE_HISUIAN, INTIMIDATE, FLASH_FIRE, ROCK_HEAD
 	db GROWTH_SLOW ; growth rate
