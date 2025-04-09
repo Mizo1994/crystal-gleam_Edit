b2ab9105eb00b157bb5a392ba969b27064220076
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2025-04-09 02:17:03 +0200

    Update geodude_alolan.asm

--- data/pokemon/base_stats/geodude_alolan.asm
+++ data/pokemon/base_stats/geodude_alolan.asm
@@ -5,7 +5,7 @@
 	db 255 ; catch rate
 	db 86 ; base exp
 	db NO_ITEM, EVERSTONE ; held items
-	dn GENDER_F50, HATCH_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for GEODUDE_ALOLAN, MAGNET_PULL, STURDY, GALVANIZE
 	db GROWTH_MEDIUM_SLOW ; growth rate
